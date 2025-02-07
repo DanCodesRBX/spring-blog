f404e28880d10bc381113d6fa02c1d0ae56000c9
DanCodesRBX <danielcw721@gmail.com>
2025-02-06 18:14:16 -0600

    a

--- src/main/java/com/icode/blog/User.java
+++ src/main/java/com/icode/blog/User.java
@@ -1,8 +1,15 @@
 package com.icode.blog;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
 
 @Entity
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
 }
