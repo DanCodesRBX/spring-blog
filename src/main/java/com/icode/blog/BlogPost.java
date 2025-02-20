a26ffaa7ae116178410384fe5280bb6fb6ea1b66
hihihihihihihiergawgwg <markmc415481@gmail.com>
2025-02-20 17:42:28 -0600

    added blogpost controller

--- src/main/java/com/icode/blog/BlogPost.java
+++ src/main/java/com/icode/blog/BlogPost.java
@@ -5,18 +5,56 @@ import jakarta.persistence.*;
 
 @Entity
 public class BlogPost {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String content;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user; // owner of the post
-
-    @ManyToMany(mappedBy = "likedPosts")
-    private List<User> likedByUsers; // list of users who liked the post
+   // list of users who liked the post
 
     // Getters and setters...
+    @Id     
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     
+    private Long id;      
+
+    private String content;      
+
+    @ManyToOne     
+    @JoinColumn(name = "user_id")     
+    private User user; // owner of the post      
+
+    @OneToOne(mappedBy = "likedPosts")     
+    int likedByUsers;
+
+    // Getter and Setter for id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for content
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    // Getter and Setter for user
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    // Getter and Setter for likedByUsers
+    int getLikedByUsers() {
+        return likedByUsers;
+    }
+
+    public void setLikedByUsers(int likedByUsers) {
+        this.likedByUsers = likedByUsers;
+    }
+    
 }
