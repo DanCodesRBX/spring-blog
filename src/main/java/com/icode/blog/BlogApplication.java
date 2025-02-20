00f970a466f0353e480e5fbc40d2b7ddeebfb372
DanCodesRBX <danielcw721@gmail.com>
2025-02-20 17:54:14 -0600

    , and an empty message aborts the commit.

--- src/main/java/com/icode/blog/BlogApplication.java
+++ src/main/java/com/icode/blog/BlogApplication.java
@@ -2,6 +2,7 @@ package com.icode.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class BlogApplication {
@@ -9,5 +10,4 @@ public class BlogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
-
 }
