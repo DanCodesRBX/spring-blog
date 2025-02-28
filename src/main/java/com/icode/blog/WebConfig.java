0848e0b8936c6a48156f2341cb0f65f54ec1251d
aniroodhanr <aniroodhanrengaa@gmail.com>
2025-02-27 18:10:05 -0600

    apple

---
+++ src/main/java/com/icode/blog/WebConfig.java
@@ -0,0 +1,14 @@
+package com.icode.blog;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/");
+    }
+}
