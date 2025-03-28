0ae05a096411818b70f3cf3dc2150ff87f58eca9
hihihihihihihiergawgwg <markmc415481@gmail.com>
2025-03-27 19:08:42 -0500

    added controller mapping for viewing specific user, edited list view for posts

--- src/main/java/com/icode/blog/controller/UserController.java
+++ src/main/java/com/icode/blog/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.icode.blog.model.BlogPost;
 import com.icode.blog.model.User;
 import com.icode.blog.service.UserService;
 
@@ -33,6 +34,16 @@ public class UserController {
     public String addPost(@ModelAttribute User user) {
         userService.save(user);
         return "redirect:/user/" + user.getId();
+    }
+
+    @GetMapping("/{id}")
+    public String viewBlotyjmnde45tgced(@PathVariable Integer id, Model model) {
+        User user = userService.findById(id);
+        if (user == null)
+            return "redirect:/users";
+
+        model.addAttribute("user", user);
+        return "user/view";
     }
 }
 
